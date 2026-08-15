7331a86a9b633684b6e1c7eeeab7490ba745fbb7
Madison-Nguyen <madnguyen070109@gmail.com>
2026-08-15 13:17:30 -0700

    audio

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const tracks = [
+    "Lofi Part 1.mp3",
+    "Lofi Part 2.mp3", "Lofi Part 3.mp3", "Lofi Part 4.mp3", "Lofi Part 5.mp3","Lofi Part 6.mp3","Lofi Part 7.mp3","Lofi Part 8.mp3", "Lofi Part 9.mp3"
+  
+  ];
 
+  const player = document.getElementById("player");
+  let current = 0;
+
+  function loadTrack(i) {
+    player.src = tracks[i];
+    player.play();
+  }
+
+  player.addEventListener("ended", () => {
+    current = (current + 1) % tracks.length;
+    loadTrack(current);
+  });
+
+  loadTrack(current);
